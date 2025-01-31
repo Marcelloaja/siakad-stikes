80a1e37bf5e636e8588b0a1c46a3ee66a2daeedd
Ignasius Tio <150216840+ignatio001@users.noreply.github.com>
2025-01-31 18:50:01 +0700

    Update Login Page 1

--- app/src/main/java/com/inbis/siakad_stikes/auth/LoginActivity.kt
+++ app/src/main/java/com/inbis/siakad_stikes/auth/LoginActivity.kt
@@ -1,23 +1,56 @@
 package com.inbis.siakad_stikes.auth
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.InputType
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.inbis.siakad_stikes.R
 import com.inbis.siakad_stikes.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private var isPasswordVisible = false
+
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginPassword.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawableEnd =
+                    binding.loginPassword.compoundDrawablesRelative[2] // Ambil icon di drawableEnd
+                if (drawableEnd != null) {
+                    val bounds = drawableEnd.bounds
+                    val clickAreaStart = binding.loginPassword.right - bounds.width() - 20
+                    val clickAreaEnd = binding.loginPassword.right
+
+                    if (event.rawX >= clickAreaStart && event.rawX <= clickAreaEnd) {
+                        togglePasswordVisibility()
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
         }
     }
+
+    private fun togglePasswordVisibility() {
+        if(isPasswordVisible) {
+            binding.loginPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            binding.loginPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0)
+        } else {
+            binding.loginPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            binding.loginPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0)
+        }
+
+        binding.loginPassword.setSelection(binding.loginPassword.text.length)
+
+        isPasswordVisible = !isPasswordVisible
+    }
+
+
 }
