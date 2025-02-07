4633e445c8be3cfb09ce7e6927feae42e84dfcdc
Marcello <marcelloilham54@gmail.com>
2025-02-07 15:31:08 +0700

    update
    layouting login

--- app/src/main/java/com/inbis/siakad_stikes/auth/LoginActivity.kt
+++ app/src/main/java/com/inbis/siakad_stikes/auth/LoginActivity.kt
@@ -1,19 +1,18 @@
 package com.inbis.siakad_stikes.auth
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
-import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import com.inbis.siakad_stikes.R
+import androidx.core.animation.doOnEnd
 import com.inbis.siakad_stikes.databinding.ActivityLoginBinding
 import com.inbis.siakad_stikes.main.ScanActivity
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
-    private var isPasswordVisible = false
 
+    private lateinit var binding: ActivityLoginBinding
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,48 +20,30 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginPassword.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                val drawableEnd =
-                    binding.loginPassword.compoundDrawablesRelative[2] // Ambil icon di drawableEnd
-                if (drawableEnd != null) {
-                    val bounds = drawableEnd.bounds
-                    val clickAreaStart = binding.loginPassword.right - bounds.width() - 20
-                    val clickAreaEnd = binding.loginPassword.right
-
-                    if (event.rawX >= clickAreaStart && event.rawX <= clickAreaEnd) {
-                        togglePasswordVisibility()
-                        return@setOnTouchListener true
-                    }
-                }
-            }
-            false
-        }
-
-        login()
-    }
-
-    private fun togglePasswordVisibility() {
-        if(isPasswordVisible) {
-            binding.loginPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            binding.loginPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0)
-        } else {
-            binding.loginPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            binding.loginPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0)
-        }
-
-        binding.loginPassword.setSelection(binding.loginPassword.text.length)
-
-        isPasswordVisible = !isPasswordVisible
-    }
-
-    private fun login() {
         binding.btnLogin.setOnClickListener {
+            animateButtonAndNavigate()
+        }
+    }
+
+    private fun animateButtonAndNavigate() {
+        val button = binding.btnLogin
+
+        val scaleDownX = ObjectAnimator.ofFloat(button, "scaleX", 0.9f)
+        val scaleDownY = ObjectAnimator.ofFloat(button, "scaleY", 0.9f)
+        val scaleUpX = ObjectAnimator.ofFloat(button, "scaleX", 1f)
+        val scaleUpY = ObjectAnimator.ofFloat(button, "scaleY", 1f)
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(scaleDownX).with(scaleDownY)
+        animatorSet.play(scaleUpX).with(scaleUpY).after(scaleDownX)
+
+        animatorSet.duration = 150
+        animatorSet.start()
+
+        animatorSet.doOnEnd {
             val intent = Intent(this, ScanActivity::class.java)
             startActivity(intent)
         }
     }
-
-
 
 }
