fcbb3e95b8c5013e59c7c9e615d308834aeb8481
Ignasius Tio <150216840+ignatio001@users.noreply.github.com>
2025-02-11 16:56:52 +0700

    Update
    Profile Fragment Trial

--- app/src/main/java/com/inbis/siakad_stikes/main/ProfileActivity.kt
+++ app/src/main/java/com/inbis/siakad_stikes/main/ProfileActivity.kt
@@ -5,15 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.inbis.siakad_stikes.R
 import com.inbis.siakad_stikes.databinding.ActivityProfileBinding
+import com.inbis.siakad_stikes.fragment.FragmentPagerAdapter
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        tabLayout = binding.tabLayout
+        viewPager2 = binding.viewPager2
+
+        adapter = FragmentPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Data Pribadi"))
+        tabLayout.addTab(tabLayout.newTab().setText("Data Akademik"))
+
+        viewPager2.adapter = adapter
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+
     }
 }
